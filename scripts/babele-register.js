68f128c17a6d99430cbcef5d4b7ab0dd8d6513f4
MyriadsOfThruths <myriads.of.truths@gmail.com>
2025-08-17 18:33:07 +0300

    v3.1.1 удалил связь с babele

--- scripts/babele-register.js
+++
@@ -1,9 +0,0 @@
-Hooks.once("init", () => {
-    if (!game.modules.get("babele")?.active) return;
-
-    Babele.get().register({
-        module: "daggerheart-ru",
-        lang: "ru",
-        dir: "packs/ru"
-    });
-});
